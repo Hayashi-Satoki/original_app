9c65c4148709c60dfeb3bf5f705372d4672eefae
Hayashi-Satoki <stk.884@gmail.com>
2021-04-29 15:25:58 +0900

    保存機能の実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,4 +2,18 @@ class PicturesController < ApplicationController
   def index
     @pictures = Picture.all
   end
+
+  def new
+    @picture = Picture.new
+  end
+   
+  def create
+    Picture.create(pictures_params)
+  end
+
+  private
+  def pictures_params
+    params.require(:picture).permit(:description, :family_id)
+  end
+
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,4 @@
 class Picture < ApplicationRecord
+  validates :description, presence: true
+  validates :family_id, presence: true
 end
